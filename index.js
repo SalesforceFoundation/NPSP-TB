df0af9c730b0f433662d92e670c8087cc72a72d1
Kevin Bromer <kbromer@salesforce.com>
2015-01-20 00:16:19 -0500

    fixed rate limiter, entry flow. added logic for posting

--- index.js
+++ index.js
@@ -1,31 +1,34 @@
 var Twit = require('twit');
-//var GitHubApi = require('github');
 var request = require('request');
-
 
 var options = {
   url: 'https://api.github.com/repos/SalesforceFoundation/Cumulus/events',
   headers: {'user-agent': 'node.js',
             'X-Poll-Interval': '60',
             'etag': ''
-            //'access_token': process.env.GITHUB_ACCESS_TOKEN
   }
 };
 
 
 var etag = '';
 var POLLING_INTERVAL = 1000;
+var modified_since = '';
 
 function callback() {
-  console.log('Setting header to: ' + etag);
+  console.log('<---------- START ---------->');
+
   options.headers['If-None-Match'] = etag;
+  options.headers['If-Modified-Since'] = modified_since;
 
   request(options, function (error, response, body) {
     if (error){
-      console.log('ERROR');
+      console.log('!!!!!!!!!!ERROR!!!!!!!!!!');
       console.log(error);
     }else{
+      console.log('Processing request response...');
       console.log(response.headers['x-ratelimit-remaining'] + ' remaining of ' + response.headers['x-ratelimit-limit'] + ' requests');
+      modified_since = options.headers['Last-Modified'];
+
       //respect the rate limit
       if (response.headers['x-poll-interval']){
         POLLING_INTERVAL =  response.headers['x-poll-interval'] * 1000;
@@ -34,26 +37,28 @@ function callback() {
         POLLING_INTERVAL = 60000;
       }
 
+      console.log('GITHUB REQUEST STATUS: ' + response.statusCode);
       if (response.statusCode == 200){
         //parse body and tweet when ready
-        console.log('Returned 200');
+
         var event_body = JSON.parse(body);
         var event_type = '';
 
-        var T = new Twit({
-          consumer_key:         process.env.CONSUMER_KEY,
-          consumer_secret:      process.env.CONSUMER_SECRET,
-          access_token:         process.env.ACCESS_TOKEN,
-          access_token_secret:  process.env.ACCESS_TOKEN_SECRET
-        });
-
         for (var i=0; i < event_body.length; i++){
-          event_type = event_body[i]['type'];
-          console.log('EVENT TYPE:' + event_type);
+          event_type = event_body[i].type;
+
+          var status_message = '';
           //always tweet a release
           if (event_type == 'ReleaseEvent'){
-            console.log('ReleaseEvent twitter post.');
-            T.post('statuses/update', { status: 'Release ' + event_body[i]['payload']['release']['name'] + " now available at " + event_body[i]['payload']['release']['html_url']}, function(err, data, response) { });
+            if (event_body[i].payload.release.prerelease === true){
+              status_message = 'A beta release ' + event_body[i].payload.release.name + " for sandboxes and DE orgs is now available at " + event_body[i].payload.release.html_url;
+            }else{
+              status_message = '' + event_body[i].payload.release.name + ' is now available! Notes & links here: ' + event_body[i].payload.release.html_url;
+            }
+
+            status_message = 'Release ' + event_body[i].payload.release.name + " now available at " + event_body[i].payload.release.html_url;
+            console.log('TWITTER POST:  ReleaseEvent ' + event_body[i].payload.release.name);
+            twitterUpdate(status_message);
 
           } else if (event_type == 'CreateEvent'){
             //not used on repo events
@@ -61,26 +66,27 @@ function callback() {
             //only tweet an issue if its new
           } else if(event_type == 'PublicEvent'){
 
-          } else if(event_type == 'PullRequestEvent' && event_body.length === 1){
-
-            //tweet a pull being merged in
-            if (event_body[i]['action'] == "closed" && event_body[i]['pull_request']['base']['merged'] == 'true'){
-              console.log('Closed pull request twitter post.');
-              T.post('statuses/update', { status: 'New code added!  Check it out: ' + event_body[i]['pull_request']['html_url'] }, function(err, data, response) { });
+          } else if(event_type == 'PullRequestEvent'){
+            //only tweet non-merge-back and closed to dev pull requests
+            var title = event_body[i].payload.pull_request.title;
+            if (event_body[i].payload.action === "closed" && event_body[i].payload.pull_request.merged === true && title.substr(0,22) !== 'Merge conflict merging'){
+              console.log('TWITTER POST: PullRequestClosed ' + title);
+              status_message = 'New code added to the main code line!  Check it out: ' + event_body[i].payload.pull_request.html_url;
+              twitterUpdate(status_message);
             }
           } else {
 
           }
         }
-
         //set the new etag
-        etag = response.headers['etag'];
+        etag = response.headers.etag;
+        modified_since = response.headers['last-modified'];
 
       } else if (response.statusCode == 304){
         //do something random if too much
         //time has gone by
-        console.log('Returned 304');
 
+/*
         //check Hub for popular questions or
         //new knowledge articles
         var nforce = require('nforce');
@@ -94,56 +100,35 @@ function callback() {
           mode: 'multi' // optional, 'single' or 'multi' user mode, multi default
         });
 
-
+*/
 
       } else {
-        console.log('RETURNED A ' + response.statusCode);
+        console.log('Unexpected response code: ' + response.statusCode);
       }
+
+      //set the schedule
+      console.log('Checking again in ' + ((POLLING_INTERVAL + 3000)/1000) + ' seconds.');
+      setTimeout(callback, POLLING_INTERVAL + 3000);
+      console.log('<---------- END ---------->');
     }
   });
-
-  console.log('SETTING TIMEOUT TO: ' + POLLING_INTERVAL);
-  setTimeout(callback, POLLING_INTERVAL);
 }
 
   setTimeout(callback, POLLING_INTERVAL);
 
+  function twitterUpdate(status_message){
+    var T = new Twit({
+      consumer_key:         process.env.CONSUMER_KEY,
+      consumer_secret:      process.env.CONSUMER_SECRET,
+      access_token:         process.env.ACCESS_TOKEN,
+      access_token_secret:  process.env.ACCESS_TOKEN_SECRET
+    });
+    T.post('statuses/update', { status: status_message },   function (err, data, response) {
 
-
-
-
-
-/*
-var github = new GitHubApi({
-  version: "3.0.0",
-  protocol: "https"
-});
-
-var msg = {headers: {}, user: 'SalesforceFoundation', repo: 'Cumulus'};
-
-github.events.getFromRepo(msg, function(error, data){
-  console.log(data.headers);
-  console.log(JSON.stringify(data));
-
-
-})*/
-
-
-
-
-
-
-/*
-
-$ curl -I https://api.github.com/users/tater/events
-HTTP/1.1 200 OK
-X-Poll-Interval: 60
-ETag: "a18c3bded88eb5dbb5c849a489412bf3"
-
-# The quotes around the ETag value are important
-$ curl -I https://api.github.com/users/tater/events \
--H 'If-None-Match: "a18c3bded88eb5dbb5c849a489412bf3"'
-HTTP/1.1 304 Not Modified
-X-Poll-Interval: 60
-
-*/
+        if (err !== null){
+          console.log('TwitterError: ' + err);
+        }else {
+          console.log('TwitterPostSuccess');
+        }
+    });
+  }
